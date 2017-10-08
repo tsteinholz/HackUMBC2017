a73562773627f0a1e6707b6a6965d487468ba299
Thomas Steinholz <tsteinholz@yahoo.com>
2017-10-07 22:51:45 -0400

    added server code

---
+++ display_server.py
@@ -0,0 +1,63 @@
+"""
+The sign display server
+by Thomas(Sascha) Alexander Steinholz
+"""
+
+from multiprocessing.connection import Listener
+from time import sleep
+from _thread import start_new_thread, allocate_lock
+
+SERVER_PORT = 7980
+parking_access_lock = allocate_lock()
+parking_directory = []
+
+def start_server():
+    """ Add new connections as they come in. """
+    server = Listener(('', SERVER_PORT))
+    print("[+] Server Created.")
+    while True:
+        connection = server.accept()
+        print("[+] Connected to Parking Sensor", str(connection.fileno()) + ".")
+        start_new_thread(client_thread, (connection,))
+
+def update_parking(identifier, direction):
+    """ Safely update the parking_directory slot count. """
+    with parking_access_lock:
+        # Find the identifier in the directory.
+        for lot in parking_directory:
+            if identifier == lot[0]:
+                if direction == 'enter':
+                    lot[1] += 1
+                    return
+                elif direction == 'leave':
+                    if lot[1] > 0:
+                        lot[1] -= 1
+                        return
+                else:
+                    print("[!] Error: Unknown message from", identifier)
+                    return
+
+        # Add the new key in the directory if not found.
+        parking_directory.append([identifier, 1 if direction == 'enter' else 0])
+
+def client_thread(conn):
+    """ Handles a client thread, """
+    while True:
+        try:
+            msg = conn.recv()
+        except EOFError:
+            print("[-] Disconnected from Parking Sensor", str(conn.fileno()) + ".")
+            conn.close()
+            return
+        print(conn.fileno(), "]", msg)
+        update_parking(conn.fileno(), msg)
+
+
+def main():
+    """ Program Entry Point. """
+    start_new_thread(start_server, ())
+    while True:
+        sleep(3)
+        print(parking_directory)
+
+main()
